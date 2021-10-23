2e3727f0fae8e7d1e23fb1efc5ce3ccd5ab3bcbf
HubertBaginski <43519818+HubertBaginski@users.noreply.github.com>
2021-10-23 22:23:08 +0200

    remvoed dataset, added requirements file, final cleanup

--- twitterFunctions/performanceMetrics.py
+++ twitterFunctions/performanceMetrics.py
@@ -2,12 +2,12 @@ from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_sc
 
 
 # function that returns performance metrics and confusion matrix / only prints to console
-def get_performance(labels, pred, classNames, score="macro"):
+def get_performance(labels, pred, class_names, score="macro"):
     f1 = f1_score(labels, pred, average=score)
     prec = precision_score(labels, pred, average=score)
     rec = recall_score(labels, pred, average=score)
     acc = accuracy_score(labels, pred)
     print("Prec: ", prec, "; Rec: ", rec, "F1: ", f1, "; Acc: ", acc)
     print(confusion_matrix(labels, pred))
-    mat = confusion_matrix(labels, pred, labels=classNames)
+    mat = confusion_matrix(labels, pred, labels=class_names)
     return mat
